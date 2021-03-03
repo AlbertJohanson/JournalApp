fc64f6dafd44bcc3ef0e9fd14e81e0e7c33d7e73
Albert Johanson <aj.dev.gt@gmail.com>
2021-03-03 13:08:35 -0600

    Adding some files

---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+
+import PropTypes from 'prop-types'
+
+import { Redirect, Route } from 'react-router-dom';
+
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => {
+
+  
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/auth/login" />}
+    />
+  );
+};
+
+PrivateRoute.prototype = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    component: PropTypes.func.isRequired
+}
