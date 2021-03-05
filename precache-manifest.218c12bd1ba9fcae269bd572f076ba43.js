32aa3d87b24456733237a3db9ba1184388775cd4
Albert Johanson <aj.dev.gt@gmail.com>
2021-03-05 17:55:54 -0600

    Updates

---
+++ precache-manifest.218c12bd1ba9fcae269bd572f076ba43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566183b40f16c6a8a329692c4d241d5a",
+    "url": "/JournalApp/index.html"
+  },
+  {
+    "revision": "93de3fc163a7f08a570d",
+    "url": "/JournalApp/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "75459cb5f3f8d90a5d0d",
+    "url": "/JournalApp/static/css/main.6366d68a.chunk.css"
+  },
+  {
+    "revision": "93de3fc163a7f08a570d",
+    "url": "/JournalApp/static/js/2.7d6ab182.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/JournalApp/static/js/2.7d6ab182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75459cb5f3f8d90a5d0d",
+    "url": "/JournalApp/static/js/main.f03a9cb7.chunk.js"
+  },
+  {
+    "revision": "9ffccb3c02a5d46c691e",
+    "url": "/JournalApp/static/js/runtime-main.cea6fe8d.js"
+  }
+]);
